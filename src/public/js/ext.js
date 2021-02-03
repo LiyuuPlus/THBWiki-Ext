fd5715949ad56ccfb11cd7447058b1f6519226b5
whuihuan <whuihuan@outlook.com>
2021-02-03 16:50:54 +0800

    新增查看背景图片，方便保存

--- src/public/js/ext.js
+++ src/public/js/ext.js
@@ -102,16 +102,46 @@ chrome.storage.local.get(['options'], (res) => {
     }
 });
 
+var getBase64ByUrl = function(src, callback, outputFormat) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', src, true);
+
+      xhr.responseType = 'arraybuffer';
+
+      xhr.onload = function(e) {
+        if (xhr.status == 200) {
+          var uInt8Array = new Uint8Array(xhr.response);
+          var i = uInt8Array.length;
+          var binaryString = new Array(i);
+          while (i--) {
+            binaryString[i] = String.fromCharCode(uInt8Array[i]);
+          }
+          var data = binaryString.join('');
+          var base64 = window.btoa(data);
+          var dataUrl = "data:" + (outputFormat || "image/png") + ";base64," + base64;
+          alert(dataUrl)
+          callback.call(this, dataUrl);
+        }
+      };
+
+      xhr.send();
+    }
+
 $().ready(() => {
     if (background && $("body").hasClass("skin-unicorn")) {
         let defurl = `${apiurl}Background.php`;
         if (custombackground) {
-            setthbextbg(custombgurl || defurl);
+            var url=custombgurl || defurl;
+            setthbextbg(url);
         }
         else {
             //根据词条判断背景
             var word = $("#firstHeading").text().replace(/ /g, "_");
-            setthbextbg(`${defurl}?char=${word}`);
+            var url=`${defurl}?char=${word}&type=1`;
+            $.get(url,{},(res)=>{
+                $("#p-namespaces ul").append($(`<li id='ca-nstab-saveBackground'><span><a href='${res}' target='_blank'>查看背景图片</a></span></li>`));
+                setthbextbg(res);
+            });
         }
     }
     if (custombanner) {
