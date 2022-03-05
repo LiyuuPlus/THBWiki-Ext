3e8e31acc8e94f628e710dcada26b9e1647913ed
whuihuan <whuihuan@outlook.com>
2022-03-06 02:32:32 +0800

    CRLF to LF

--- src/popup/components/functions.js
+++ src/popup/components/functions.js
@@ -2,24 +2,24 @@ import { getLang } from "../../js/browser/api";
 
 export default {
     "UnreadTab": {
-        name:getLang("UnreadNotification"),
-        type:"page",
+        name: getLang("UnreadNotification"),
+        type: "page",
         sort: 0,
         src: "./Tabs/UnreadTab.vue",
         function: null,
         enabled: true
     },
     "RemindTab": {
-        name:getLang("RemindNotification"),
-        type:"page",
-        sort:1,
+        name: getLang("RemindNotification"),
+        type: "page",
+        sort: 1,
         src: "./Tabs/RemindTab.vue",
         function: null,
         enabled: true
     },
     "MsgTab": {
-        name:getLang("MsgNotification"),
-        type:"page",
+        name: getLang("MsgNotification"),
+        type: "page",
         sort: 2,
         src: "./Tabs/MsgTab.vue",
         function: null,
